f7b1f24bf58ec9655ca9d8321bdec6e3a2f014f0
kangjae4real <awpe89@gmail.com>
2026-05-12 21:44:35 +0900

    refactor: fix format_duration function parameter signature

--- src/auto_watch/util.py
+++ src/auto_watch/util.py
@@ -1,8 +1,11 @@
 """범용 헬퍼"""
 
 
-def format_duration(total_sec: int) -> str:
-    """초를 H:MM:SS (1시간 이상) 또는 M:SS 형식으로 변환"""
+def format_duration(total_sec: int | float) -> str:
+    """초를 H:MM:SS (1시간 이상) 또는 M:SS 형식으로 변환.
+
+    float 입력(예: HTMLVideoElement.duration)을 허용하며 내부에서 int로 절삭.
+    """
     total_sec = int(total_sec)
     total_m, s = divmod(total_sec, 60)
     h, m = divmod(total_m, 60)
